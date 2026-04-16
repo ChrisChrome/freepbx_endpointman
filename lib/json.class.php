5b594060df363e5470277b5db3150e56e7599849
ChrisChrome <chris@chrischro.me>
2026-04-16 10:27:13 -0600

    Gwug

--- lib/json.class.php
+++ lib/json.class.php
@@ -231,7 +231,7 @@ if(!class_exists('Services_JSON')) {
 	            return mb_convert_encoding($utf16, 'UTF-8', 'UTF-16');
 	        }
 
-	        $bytes = (ord($utf16{0}) << 8) | ord($utf16{1});
+	        $bytes = (ord($utf16[0]) << 8) | ord($utf16[1]);
 
 	        switch(true) {
 	            case ((0x7F & $bytes) == $bytes):
